3047c7ea9362b60ffb0b0c33e5250defa7c32591
Alessio Iafrate <alessioiafrate@hotmail.com>
2016-08-07 00:18:32 +0200

    Added DeviceFamilyTrigger and used in sample project

---
+++ XboxHelpers/Triggers/DeviceFamilyTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace XboxHelpers.Triggers
+{
+    public class DeviceFamilyTrigger : StateTriggerBase
+
+    {
+
+        public string DeviceFamily
+
+        {
+
+            get { return (string)GetValue(DeviceFamilyProperty); }
+
+            set { SetValue(DeviceFamilyProperty, value); }
+
+        }
+
+
+
+        public static readonly DependencyProperty DeviceFamilyProperty =
+
+            DependencyProperty.Register("DeviceFamily", typeof(string), typeof(DeviceFamilyTrigger),
+
+                new PropertyMetadata(string.Empty, DeviceFamilyChanged));
+
+
+
+        private static void DeviceFamilyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+
+        {
+
+            var @this = (DeviceFamilyTrigger)dependencyObject;
+
+            var queriedFamily = (string)dependencyPropertyChangedEventArgs.NewValue;
+
+            var currentFamily = Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily;
+
+            @this.SetActive(queriedFamily == currentFamily);
+
+        }
+    }
+}
